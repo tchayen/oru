3d9281bf6f732316774b191f2c226eec9d80112d
Tomek Czajecki <tomek.czajecki@gmail.com>
2026-02-16 19:16:05 +0200

    cli: Replace qrcode-terminal with maintained qrcode package (#125)

--- cli/src/qrcode-terminal.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "qrcode-terminal" {
-  interface QRCodeOptions {
-    small?: boolean;
-  }
-  function generate(input: string, opts?: QRCodeOptions, callback?: (code: string) => void): void;
-  function generate(input: string, callback?: (code: string) => void): void;
-  function setErrorLevel(level: string): void;
-  export default { generate, setErrorLevel };
-}
